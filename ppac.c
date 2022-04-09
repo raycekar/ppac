cad43199535da01c7f762e912868aa303ccd5694
raycekar <raycekarneal@gmail.com>
2022-04-08 23:12:14 -0500

    Added if package is installed function

--- ppac.c
+++ ppac.c
@@ -4,7 +4,7 @@
 
 
 typedef struct Package {
-   char *pkg;
+   char pkg[50];
    struct Package *next;
 } Package;
 
@@ -16,24 +16,46 @@ const char* combineStrings(const char *first, const char *second){
    return temp;
 }
 
+int isPackageInstalled(char *pkg){
+   char cmd[100] = "pacman -Qi ";
+   strcat(cmd, pkg);
+   char output[100];
+   output[0] = '\0';
+   FILE *p;
+   p = popen(cmd, "r");
+   fgets(output, 100, p);
+   if(
+      output[0] == 'N' &&
+      output[1] == 'a' &&
+      output[2] == 'm' &&
+      output[3] == 'e'
+      ){
+      return 1;
+   }
+   return 0;
+
+}
 
 int main(int argc, char *argv[]) {
    if (argc != 2){
       printf("Error: ppac only accepts one agument.\n");
       return 1;
    }
-   const char *pacman = "pacman -Si ";
-   const char *command = combineStrings(pacman, argv[1]);
 
-   FILE *p;
-   p = popen(command, "r");
-
-   int ch;
-   while( (ch=fgetc(p)) != EOF)
-        putchar(ch);
-    pclose(p);
+   printf("%d", isPackageInstalled(argv[1]));
    
-   Package head;
-   head->pkg = "hello";
+   // const char *pacman = "pacman -Qi ";
+   // const char *command = combineStrings(pacman, argv[1]);
+
+   // FILE *p;
+   // p = popen(command, "r");
+
+   // int ch;
+   // while( (ch=fgetc(p)) != EOF)
+   //      putchar(ch);
+   //  pclose(p);
+   
+   // Package head;
+   // strcpy(head.pkg, "hello");
    return 0;
 }
